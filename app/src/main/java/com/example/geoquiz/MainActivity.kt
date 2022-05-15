52d5f48de891a3675ce502c967b00c8fc24b2274
Henry Cai <alaba.havai@gmail.com>
2022-05-15 19:11:13 +0800

    加入viewBinding

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -4,26 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.viewbinding.ViewBinding
+import com.example.geoquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     //使用lateinit修飾符號告訴編譯器，會提供非空值的view值
-    private lateinit var trueButton:Button
-    private lateinit var falseButton:Button
+    private lateinit var viewBinding:ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
         //在onCreate()方法調用 setContentView後，所有的視圖物件才會被放入到手機內存中
-        setContentView(R.layout.activity_main)
+        setContentView(viewBinding.root)
 
         initView()
     }
 
     private fun initView() {
-        //使用資源ID獲取視圖物件
-          trueButton  = findViewById(R.id.true_button)
-          falseButton = findViewById(R.id.false_button)
         //設置監聽器
-        trueButton.setOnClickListener {
+        viewBinding.trueButton.setOnClickListener {
             Toast.makeText(
                 this,
                 R.string.correct_toast,
@@ -31,7 +32,7 @@ class MainActivity : AppCompatActivity() {
             ).show()
         }
 
-        falseButton.setOnClickListener {
+        viewBinding.falseButton.setOnClickListener {
             Toast.makeText(
                 this,
                 R.string.incorrect_toast,
