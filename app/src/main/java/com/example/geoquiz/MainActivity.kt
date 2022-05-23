05c024f359d2718045e05e492fb7b0e8c3c6749f
Henry Cai <alaba.havai@gmail.com>
2022-05-23 15:42:59 +0800

    Add Activity LifeCycle callback function

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.viewbinding.ViewBinding
 import com.example.geoquiz.databinding.ActivityMainBinding
+
+private  val TAG = MainActivity::class.java.simpleName as String
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,11 +28,37 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, " onCreate(Bundle?) called")
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         //在onCreate()方法調用 setContentView後，所有的視圖物件才會被放入到手機內存中
         setContentView(viewBinding.root)
 
         initView()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 
     private fun initView() {
